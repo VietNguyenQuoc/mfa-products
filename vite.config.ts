6ad3218d9dacd7f848d345cb1f30b31c1fea384a
Viet Nguyen <nqviet157@gmail.com>
2025-03-24 22:31:18 +0700

    externalize react

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "https://mfa-products-production.up.railway.app/",
+  build: {
+    rollupOptions: {
+      external: ["react", /react-dom/],
+    },
+  },
 });
