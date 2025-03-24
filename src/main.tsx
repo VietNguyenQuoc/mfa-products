1bbe54d1b63c3b7b87290b7c5334f465a4429953
Viet Nguyen <nqviet157@gmail.com>
2025-03-24 22:18:29 +0700

    clean up light dom render

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-createRoot(
-  document.getElementById(import.meta.env.VITE_MINIAPP_NAME || "root")!
-).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
 class ProductElement extends HTMLElement {
   connectedCallback() {
     createRoot(this.attachShadow({ mode: "open" })).render(
